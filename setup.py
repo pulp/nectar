1aa74729ddefa6cae418fa30a32a10b131f9b007
Jason L Connor <jason.connor@gmail.com>
2013-05-01 12:39:38 -0600

    Automatic commit of package [python-nectar] release [0.90.2-1].

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='nectar',
-      version='0.90.0',
+      version='0.90.2',
       url='https://github.com/pulp/nectar',
       description='Performance tuned network download client library',
       license='GPLv2',
