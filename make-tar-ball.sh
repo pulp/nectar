b98d448213642942b546baf76c18fc7ec023e306
Jason L Connor <jason.connor@gmail.com>
2013-05-14 11:18:38 -0600

    actuall script :)

--- make-tar-ball.sh
+++ make-tar-ball.sh
@@ -1,1 +1,10 @@
-git archive --format=tar.gz --prefix=python-nectar-0.90.0/ master >python-nectar-0.90.0.tar.gz
+#!/usr/bin/sh
+
+# usage: ./make-tar-ball.sh <branch> <version>
+# e.g.: ./make-tar-ball.sh master 0.90.0 -> python-nectar-0.90.0.tar.gz
+
+BRANCH=$1
+VERSION=$2
+GIT=$(which git)
+
+$GIT archive --format=tar.gz --prefix=python-nectar-$VERSION/ $BRANCH >python-nectar-$VERSION.tar.gz
