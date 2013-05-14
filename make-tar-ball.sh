3f23da97faa216242428b6e0f342d69a75e9c2aa
Jason L Connor <jason.connor@gmail.com>
2013-05-14 13:51:31 -0600

    added comment

--- make-tar-ball.sh
+++ make-tar-ball.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 
+# This script exists to ease the step of making a tar ball that is compatible
+# with the rpm spec file.
+
 # usage: ./make-tar-ball.sh <branch> <version>
 # e.g.: ./make-tar-ball.sh master 0.90.0 -> python-nectar-0.90.0.tar.gz
 
