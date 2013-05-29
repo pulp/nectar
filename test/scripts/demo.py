33c7b242f2287493676d32e445b918acef563ac8
Jason L Connor <jason.connor@gmail.com>
2013-05-29 10:34:03 -0600

    added more scripts, and gitignore so that we do not add sensitive credentials by accident

---
+++ test/scripts/demo.py
@@ -0,0 +1,19 @@
+# run 'python -i demo.py' and see the result. You should see 'start' printed
+# and then 'finish'. Then uncomment the monkey patch line and try again. You
+# will see the 'start', but never the 'finish'.
+
+import threading
+import time
+
+import eventlet
+eventlet.monkey_patch()
+
+def foo():
+    print 'start'
+    time.sleep(1)
+    print 'finish'
+
+thread = threading.Thread(target=foo)
+thread.start()
+thread.join()
+
