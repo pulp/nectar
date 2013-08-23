77de89b33c44359711ceb8eb93ee3d9e45388fd1
Jason L Connor <jason.connor@gmail.com>
2013-08-23 09:12:56 -0600

    bumped nectar version to 1.1

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2013, Pulp Team'
 # built documents.
 #
 # The short X.Y version.
-version = '0.97.0'
+version = '1.1.0'
 # The full version, including alpha/beta/rc tags.
 #release = '2'
 release = version
