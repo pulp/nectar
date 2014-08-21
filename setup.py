c2d8e132bb8740b2cba401805a951b4be037b6c1
Barnaby Court <bcourt@redhat.com>
2014-08-21 13:30:10 -0400

    Automatic commit of package [python-nectar] release [1.3.1-1].

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 setup(name='nectar',
-      version='1.3.0',
+      version='1.3.1',
       url='https://github.com/pulp/nectar',
       description='Performance tuned network download client library',
       license='GPLv2',
