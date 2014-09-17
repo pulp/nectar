83ef0c3e2529b9a2eb08c2638327ea41c5ca1210
asmacdo <asmacdo@gmail.com>
2014-09-17 17:29:36 -0400

    Handle local IOErrors in debug level logs rather than error level.

--- nectar/downloaders/local.py
+++ nectar/downloaders/local.py
@@ -172,6 +172,10 @@ class LocalFileDownloader(Downloader):
                 self.fire_download_progress(report)
                 last_progress_update = now
 
+        except IOError, e:
+            logger.debug(e)
+            report.error_msg = str(e)
+            report.download_failed()
         except Exception, e:
             logger.exception(e)
             report.error_msg = str(e)
