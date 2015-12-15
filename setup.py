6206e088bdd50913de53581d62f68788fe05c8ca
Ina Panova <ipanova@redhat.com>
2015-12-15 15:48:56 +0100

    Automatic commit of package [python-nectar] release [1.4.2-1].

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='nectar',
-      version='1.4.1',
+      version='1.4.2',
       url='https://github.com/pulp/nectar',
       description='Performance tuned network download client library',
       license='GPLv2',
